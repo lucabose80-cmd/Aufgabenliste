c1ea7a2b157649a0a50e314c04c652706becf7cb
lucabose80-cmd <lucabose80@gmail.com>
2026-06-17 16:38:31 +0200

    Update all icons to any maskable to force Firefox Android full bleed

--- vite.config.js
+++ vite.config.js
@@ -26,19 +26,19 @@ export default defineConfig({
             src: 'pwa-192x192.png',
             sizes: '192x192',
             type: 'image/png',
-            purpose: 'any'
+            purpose: 'any maskable'
           },
           {
             src: 'pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'any'
+            purpose: 'any maskable'
           },
           {
             src: 'maskable-icon-512x512.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'maskable'
+            purpose: 'any maskable'
           }
         ]
       }
